609dab6a103cf02a0eff87a7e93709afc686e28e
root <showell30@yahoo.com>
2023-05-08 23:43:21 +0000

    progress

--- unread_count_helpers.js
+++ unread_count_helpers.js
@@ -1,6 +1,4 @@
 zulip.unread_count_helpers = (function () {
-    const { setStyles } = zulip.style_helpers;
-
     function build_unread_count() {
         const elem = document.createElement("span");
         elem.classList.add("style-panel-unread-counter");
