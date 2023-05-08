7b6957cbedceec4e65dee0c65f32cdaba3dcd661
root <showell30@yahoo.com>
2023-05-08 13:51:56 +0000

    progress

--- all_messages.js
+++ all_messages.js
@@ -1,6 +1,10 @@
 window.all_messages_component = (function () {
     const { build_top_left_panel_icon } = window.icon_helpers;
-    const { style_main_link, setStyles } = window.style_helpers;
+    const {
+        colorize_main_link,
+        style_list_item,
+        style_main_link,
+    } = window.style_helpers;
 
     function build_all_messages() {
         function build_main_link(li) {
@@ -43,20 +47,19 @@ window.all_messages_component = (function () {
     }
 
     function style(all_messages) {
-        setStyles(all_messages.li, {
-            paddingLeft: "10px",
-            paddingRight: "10px",
-            paddingTop: "1px",
-            paddingBottom: "1px",
-        });
-
+        style_list_item(all_messages.li);
         style_main_link(all_messages.main_link);
+    }
+
+    function colorize(all_messages) {
+        colorize_main_link(all_messages.main_link);
     }
 
     function fully_build({ services }) {
         const all_messages = build_all_messages();
         wire_up_handlers({ all_messages, services });
         style(all_messages);
+        colorize(all_messages);
 
         return all_messages;
     }
--- style_helpers.js
+++ style_helpers.js
@@ -31,12 +31,28 @@ window.style_helpers = (function () {
         setStyles(a, {
             marginTop: "1px",
             display: "block",
+        });
+    }
+
+    function colorize_main_link(a) {
+        setStyles(a, {
             color: "rgb(51, 51, 51)",
+        });
+    }
+
+    function style_list_item(li) {
+        setStyles(li, {
+            paddingLeft: "10px",
+            paddingRight: "10px",
+            paddingTop: "1px",
+            paddingBottom: "1px",
         });
     }
 
     return {
         setStyles,
+        colorize_main_link,
         style_main_link,
+        style_list_item,
     };
 })();
