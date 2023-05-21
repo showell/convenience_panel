656281ded6e8e885fad39eef9252394cbea31309
root <showell30@yahoo.com>
2023-05-21 15:50:52 +0000

    progress

--- ui.js
+++ ui.js
@@ -21,37 +21,39 @@ import { build_handlers } from "./handlers.js";
         panel.repopulate_text({ translate: services.translate });
     }
 
-    const french_button = make_test_button({
+    make_test_button({
         label: "French",
         action: translate_to_french,
     });
 
-    const unread_button = document.createElement("button");
-    unread_button.innerText = "Set unreads";
-
-    unread_button.addEventListener("click", () => {
+    function set_unreads() {
         panel.update_unread_count({
             all_messages: 10,
             mentions: 20,
             starred_messages: 30,
             drafts: 40,
         });
+    }
+
+    make_test_button({
+        label: "Set unread counts",
+        action: set_unreads,
     });
+    
 
-    const clear_unreads_button = document.createElement("button");
-    clear_unreads_button.innerText = "Clear unreads";
-
-    clear_unreads_button.addEventListener("click", () => {
+    function clear_unreads() {
         panel.update_unread_count({
             all_messages: 0,
             mentions: 0,
             starred_messages: 0,
             drafts: 0,
         });
-    });
+    }
 
-    document.body.append(unread_button);
-    document.body.append(clear_unreads_button);
+    make_test_button({
+        label: "Clear unread counts",
+        action: clear_unreads,
+    });
 
     window.panel = panel;
 }
